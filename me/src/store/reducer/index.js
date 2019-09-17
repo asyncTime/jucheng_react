b15e62bc6025dac8d8caada4f393c35618a0323b
MyGirs <522630myk456@sina.com>
2019-09-17 19:07:11 +0800

    合并好的

--- me/src/store/reducer/index.js
+++ me/src/store/reducer/index.js
@@ -1,16 +1,13 @@
 import {
     combineReducers
 }from "redux"
-<<<<<<< HEAD
+
 import {navImageList,PriorityIn} from "./navImage"
-export default combineReducers({
-    navImageList,
-    PriorityIn
-=======
-import navImageList from "./navImage"
 import getIntegralList from "./integral"
 export default combineReducers({
     navImageList,
+    PriorityIn,
     getIntegralList
->>>>>>> 4772083b3846498582e30c27efb2196ff1e54d08
+
 })
+
