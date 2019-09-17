be1ffca1c22d1ae05cc60e2f171b4214221c2d05
asyncTime <asyncme@126.com>
2019-09-17 21:00:14 +0800

    张石亮-09:00

---
+++ me/src/store/actionCreact/integral/integralShop/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+export const changeScoresCityList = function (payload) {
+    return {
+        type:"changeScoresCityList",
+        payload
+    }
+}
+export const changeAllScoresList = function (payload) {
+    return {
+        type:"changeAllScoresList",
+        payload
+    }
+}
+export default {
+    getScoresCityList(){
+       return async (dispatch) => {
+           const {data} = await axios.post("/ju/Scores/getScoresCityList")
+           console.log(data)
+           dispatch(changeScoresCityList(data))
+       }
+    },
+    getAllScoresList() {
+        return async (dispatch) => {
+            const {data} = await axios.post("/ju/Scores/getAllScoresList");
+            console.log(data)
+            dispatch(changeAllScoresList(data))
+        }
+    }
+}
---
+++ me/src/store/state/integral/integralShop/index.js
@@ -0,0 +1,4 @@
+export default {
+    scoresCityList:[],
+    AllScoresList:[]
+}
