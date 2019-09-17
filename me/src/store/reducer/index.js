c4e0ac957cb15048cceb4ccf716d07ad5859a0e2
asyncTime <asyncme@126.com>
2019-09-17 15:31:54 +0800

    0917/03:30没弄什么功能

--- me/src/store/reducer/index.js
+++ me/src/store/reducer/index.js
@@ -2,6 +2,8 @@ import {
     combineReducers
 }from "redux"
 import navImageList from "./navImage"
+import integralList from "./integral"
 export default combineReducers({
-    navImageList
+    navImageList,
+    integralList
 })
---
+++ me/src/store/reducer/integral/index.js
@@ -0,0 +1,9 @@
+import initState from "../../state/integral";
+import {get_integral_list} from "../../actionType/integral";
+export default function (state=initState,{type,payload}){
+    state = JSON.parse(JSON.stringify(state));
+    if(type === get_integral_list){
+        state.integralList = payload;
+    }
+    return state;
+}
